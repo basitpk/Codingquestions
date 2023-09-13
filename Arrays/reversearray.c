6b6441671ac6d28c35b2cdc5bf7e503b0f5121da
basitpk <kalalbasit@gmail.com>
2023-09-13 21:09:57 +0530

    Created Arrays section

---
+++ Arrays/reversearray.c
@@ -0,0 +1,24 @@
+'''
+Problem Statement :
+   Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+'''
+
+
+void reverse(int *val, int l, int r)
+{
+    int temp=0;
+    while(l < r)
+    {
+        temp = val[l];
+        val[l]=val[r];
+        val[r]=temp;
+        l++;
+        r--;
+    }
+}
+void rotate(int* nums, int numsSize, int k){
+    int rot=k%numsSize;
+    reverse(nums, 0, numsSize -rot -1 );
+    reverse(nums, numsSize-rot,numsSize -1);
+    reverse(nums,0, numsSize-1);
+}
---
+++ Arrays/singlenum.py
@@ -0,0 +1,18 @@
+'''
+  Problem Statement
+  Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+  
+  You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+'''
+
+
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res =0
+        for i in range(0,len(nums)):
+            res ^= nums[i]
+        return res
