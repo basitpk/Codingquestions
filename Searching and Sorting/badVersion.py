a98b579582b1ee888fab7fba4f80ca5576da1c63
basitpk <kalalbasit@gmail.com>
2023-10-20 19:43:41 +0530

    New sort merge sort Added

--- Searching and Sorting/badVersion.py
+++ Searching and Sorting/badVersion.py
@@ -16,6 +16,12 @@ call isBadVersion(3) -> false
 call isBadVersion(5) -> true
 call isBadVersion(4) -> true
 Then 4 is the first bad version.
+
+----------------------------------------  
+ Note : The problem Uses binary search  
+ ---------------------------------------
+
+
 '''
 
 
