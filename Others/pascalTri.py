b5a5bc74efc4a7aa3fc17ebb76fac455e862ec2e
basitpk <kalalbasit@gmail.com>
2024-01-26 19:12:37 +0530

    Added pascals triangle nth row

--- Others/pascalTri.py
+++ Others/pascalTri.py
@@ -32,3 +32,20 @@ class Solution(object):
             # index +=1
         return output
         
+
+ '''
+ 
+Get nth row of Pascals triangle
+
+ class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row=[]
+        if (rowIndex == 0):
+            return [1]
+        elif(rowIndex == 1):
+            return [1,1]
+        else :
+            row = self.getRow(rowIndex-1)
+            return [1] + [ row[i]+ row[i-1] for i in range(1,rowIndex) ] + [1]
+
+ '''       
